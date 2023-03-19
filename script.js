ab7c9ba9a1c2e60a56b894f36a629d8a0b009222
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-19 23:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-//your JS code here. If required.
+async function fetchData(callback) {
+  const response = await fetch(url);
+  const data = await response.json();
+  callback(data.title);
+}
+
+document.getElementById('btn').addEventListener('click', async function() {
+  fetchData((result) => {
+    document.getElementById('output').textContent = result;
+  });
+});
