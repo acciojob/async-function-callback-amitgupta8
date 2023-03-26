cecad4af2d944f01f024cae917e9e9a8f8c12296
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-26 23:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
-
-async function fetchData(callback) {
-  const response = await fetch(url);
-  const data = await response.json();
-  callback(data.title);
-}
-
-document.getElementById('btn').addEventListener('click', async function() {
-  fetchData((result) => {
-    document.getElementById('output').textContent = result;
-  });
-});
+const url = "https://jsonplaceholder.typicode.com/posts/1"; 
+  
+ //your JS code here. If required. 
+  
+ async function fetchData(callback) { 
+   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1"); 
+   const data = await response.json(); 
+   callback(data); 
+ } 
+  
+ function displayMessage(data) { 
+         document.getElementById("output").innerText = data.title; 
+ } 
+  
+ // fetchData(displayMessage); 
+ // let sample = fetchData(displayMessage); 
+ // let btn1 = document.querySelector("#btn"); 
+ // btn1.addEventListener("click", displayMessage);
